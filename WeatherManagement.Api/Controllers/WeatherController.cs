a3701d03f88df60022ef9e7870af6ffd5fb70f45
Azamat Bayev <azamat.bayeav1@gmail.com>
2021-01-22 18:02:39 +0100

    Small chnages to Db connection and get functions

--- WeatherManagement.Api/Controllers/WeatherController.cs
+++ WeatherManagement.Api/Controllers/WeatherController.cs
@@ -1,28 +1,37 @@
 ﻿
-//using Microsoft.AspNetCore.Mvc;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using Microsoft.AspNetCore.Http;
-//using WeatherManagement.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using WeatherManagement.Api.Models;
 
-//namespace WeatherManagement.Api.Controllers
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class WeatherController : ControllerBase
-//    {
-//        private readonly IWeatherDataRepository weatherDataRepository;
+namespace WeatherManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        private readonly IWeatherDataRepository weatherDataRepository;
 
-//        public WeatherController (IWeatherDataRepository weatherDataRepository)
-//        {
-//            this.weatherDataRepository = weatherDataRepository;
-//        }
-//        [HttpGet]
-//        public async Task<AcitionResult> GetWeatherData()
-//        {
-//            return Ok(await weatherDataRepository.GetWeatherData());
-//        }
-//    }
-//}
+        public WeatherController(IWeatherDataRepository weatherDataRepository)
+        {
+            this.weatherDataRepository = weatherDataRepository;
+        }
+        [HttpGet]
+        public async Task<ActionResult> GetWeatherData()
+        {
+            try
+            {
+                return Ok(await weatherDataRepository.GetWeatherData());
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from database.");
+            }
+            
+        }
+    }
+}
--- weatherAppBS/obj/Debug/netcoreapp3.1/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ weatherAppBS/obj/Debug/netcoreapp3.1/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -13,63 +13,63 @@ namespace weatherAppBS.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\bayev\Desktop\dev\weatherAppBS\weatherAppBS\_Imports.razor"
+#line 1 "C:\Users\Azama\Desktop\code\dotnet\plurarSight\DaysOfWeek\weatherAppBS\weatherAppBS\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\bayev\Desktop\dev\weatherAppBS\weatherAppBS\_Imports.razor"
+#line 2 "C:\Users\Azama\Desktop\code\dotnet\plurarSight\DaysOfWeek\weatherAppBS\weatherAppBS\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\bayev\Desktop\dev\weatherAppBS\weatherAppBS\_Imports.razor"
+#line 3 "C:\Users\Azama\Desktop\code\dotnet\plurarSight\DaysOfWeek\weatherAppBS\weatherAppBS\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\bayev\Desktop\dev\weatherAppBS\weatherAppBS\_Imports.razor"
+#line 4 "C:\Users\Azama\Desktop\code\dotnet\plurarSight\DaysOfWeek\weatherAppBS\weatherAppBS\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\bayev\Desktop\dev\weatherAppBS\weatherAppBS\_Imports.razor"
+#line 5 "C:\Users\Azama\Desktop\code\dotnet\plurarSight\DaysOfWeek\weatherAppBS\weatherAppBS\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\bayev\Desktop\dev\weatherAppBS\weatherAppBS\_Imports.razor"
+#line 6 "C:\Users\Azama\Desktop\code\dotnet\plurarSight\DaysOfWeek\weatherAppBS\weatherAppBS\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\bayev\Desktop\dev\weatherAppBS\weatherAppBS\_Imports.razor"
+#line 7 "C:\Users\Azama\Desktop\code\dotnet\plurarSight\DaysOfWeek\weatherAppBS\weatherAppBS\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\bayev\Desktop\dev\weatherAppBS\weatherAppBS\_Imports.razor"
+#line 8 "C:\Users\Azama\Desktop\code\dotnet\plurarSight\DaysOfWeek\weatherAppBS\weatherAppBS\_Imports.razor"
 using weatherAppBS;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\bayev\Desktop\dev\weatherAppBS\weatherAppBS\_Imports.razor"
+#line 9 "C:\Users\Azama\Desktop\code\dotnet\plurarSight\DaysOfWeek\weatherAppBS\weatherAppBS\_Imports.razor"
 using weatherAppBS.Shared;
 
 #line default
